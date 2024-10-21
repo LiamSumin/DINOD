3359aa37fa983373d0abd4ff3b16191c18c57705
sumin <tnals4026@chungbuk.ac.kr>
2024-10-21 13:50:33 +0200

    FIRST COMMIT

---
+++ main.py
@@ -0,0 +1,63 @@
+import torch
+import copy
+import time
+from model.backbone.dinov2.vision_transformer import vit_small, vit_base, vit_large, vit_giant2
+
+_default_config = {
+    'block_chunks': 0,
+    'init_values': 1.0e-05,
+    'drop_path_uniform': True,
+    'img_size': 518
+}
+config = copy.deepcopy(_default_config)
+
+
+if __name__ == "__main__":
+    latencies = []
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+
+    model = vit_small(patch_size=14, **config).to(device)
+
+
+    model.load_state_dict(torch.load("pretrained/dinov2_vits14_pretrain.pth", weights_only=True))
+    tmp = torch.rand((1, 3, 518,518)).to(device)
+
+    for _ in range(10):  # 10번의 Warmup Forward Pass
+        with torch.no_grad():
+            tmp_warmup = model.patch_embed(tmp)
+            for block in model.blocks:
+                tmp_warmup = block(tmp_warmup)
+            tmp_warmup = model.norm(tmp_warmup)
+            tmp_warmup = model.head(tmp_warmup)
+
+    print()
+    torch.cuda.synchronize()
+    s_t_blocks = time.time()
+    tmp = model.patch_embed(tmp)
+    torch.cuda.synchronize()
+    latencies.append(1000*(time.time() - s_t_blocks))
+    for i in range(len(model.blocks)):
+        torch.cuda.synchronize()
+
+        s_t_blocks = time.time()
+        tmp = model.blocks[i](tmp)
+        torch.cuda.synchronize()
+
+        latencies.append(1000*(time.time() - s_t_blocks))
+    torch.cuda.synchronize()
+
+    s_t_blocks = time.time()
+    tmp = model.norm(tmp)
+    torch.cuda.synchronize()
+
+    latencies.append(1000 * (time.time() - s_t_blocks))
+    torch.cuda.synchronize()
+
+    s_t_blocks = time.time()
+    tmp = model.head(tmp)
+    torch.cuda.synchronize()
+
+    latencies.append(1000 * (time.time() - s_t_blocks))
+    print(latencies)
+    print(sum(latencies))
+    print(tmp.shape)
